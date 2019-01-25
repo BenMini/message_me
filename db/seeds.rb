798baa2a84926eda5f925af97f4ff254d9cdad85
Ben Minifie <benrs6mini@gmail.com>
2019-01-24 19:01:39 -0500

    Display actual messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Ben", password: "password")
+User.create(username: "Lisa", password: "password")
+User.create(username: "Jake", password: "password")
+User.create(username: "Maggie", password: "password")
+
+Message.create(body: "Hi this is another message", user: User.first)
+Message.create(body: "Hi this is another message", user: User.first)
+Message.create(body: "Hi this is another message", user: User.first)
